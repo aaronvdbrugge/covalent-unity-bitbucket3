c84c56504599685a5be47808852223ab693dee43
UltimateWalrus <ultimatewalrus@gmail.com>
2021-09-20 21:29:32 -0700

    Integrated SRDebugger, which will be useful when people encounter bugs on device. Whenever you encounter a bug, tap the upper left corner three times and check the debug console to see if there wer any errors.
    In addition, SRDebugger has been configured so you can set max player speed from the Options screen. This was intended to help fix a bug with differing player speed on mobile, but I think I may have fixed the bug already.

---
+++ Assets/Covalent/Scripts/Player/SROptions.Player_Movement.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using SRDebugger;
+
+/// <summary>
+/// Allows player speed to be configured from SRDebugger.
+/// </summary>
+public partial class SROptions {
+
+	// Allows setting of player max speed...
+	[Category("Movement")] 
+	public float PlayerMaxSpeed {
+		get => Player_Controller_Mobile.mine.playerMovement.maxSpeed;
+		set => Player_Controller_Mobile.mine.playerMovement.maxSpeed = value;
+	}
+}
