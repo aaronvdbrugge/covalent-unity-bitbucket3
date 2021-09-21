160b251beaa29035156aaa02dcaae4b5f6d10bbe
UltimateWalrus <ultimatewalrus@gmail.com>
2021-09-21 12:19:49 -0700

    Added button for SRDebugger. Turned off isometric movement skew
    Former-commit-id: 614428db7248904d8134cd94fb7dd80e6e484d35

---
+++ Assets/Covalent/Scripts/Util/StartSRDebugger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+/// <summary>
+/// Can connect a button to this
+/// </summary>
+public class StartSRDebugger : MonoBehaviour
+{
+	public void DoStart()
+	{
+		SRDebug.Instance.ShowDebugPanel(false);
+	}
+}
