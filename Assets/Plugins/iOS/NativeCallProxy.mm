05a3cb74314824e8e34cfc148fad115866a0ac87
Sebastian Janisz <admin@ultimatewalrus.com>
2021-10-05 17:17:47 -0700

    Added externs into NativeCallProxy.mm

--- Assets/Plugins/iOS/NativeCallProxy.mm
+++ Assets/Plugins/iOS/NativeCallProxy.mm
@@ -8,8 +8,11 @@ id<NativeCallsProtocol> api = NULL;
 }
 @end
 extern "C" {
+
 void showHostMainWindow(const char* color) { return [api showHostMainWindow:[NSString stringWithUTF8String:color]]; }
 void _updatePlayersInRoom(char *arr[], int count) {
+    NSLog(@"FROM UNITY: updatePlayersInRoom(%i)", count);
+    
     char* cStringCopy(const char* string);
     NSMutableArray *items = [NSMutableArray array];
     for(int i = 0; i < count; i++)
@@ -19,4 +22,31 @@ void _updatePlayersInRoom(char *arr[], int count) {
     }
     [api updatePlayersInRoom:items];
 }
+void _playerDidMute(unsigned int player_id) {
+    // NOTE! player_id is an Agora assigned ID.
+    NSLog(@"FROM UNITY: playerDidMute(%i)", player_id);
+}
+void _playerDidUnmute(unsigned int player_id) {
+    NSLog(@"FROM UNITY: playerDidUnmute(%i)", player_id);
+}
+void _playerStartedTalking(unsigned int player_id) {
+    NSLog(@"FROM UNITY: playerStartedTalking(%i)", player_id);
+}
+void _playerEndedTalking(unsigned int player_id) {
+    NSLog(@"FROM UNITY: playerEndedTalking(%i)", player_id);
+}
+void _playerDidLeaveGame() {
+    NSLog(@"FROM UNITY: _playerDidLeaveGame");
+}
+void _failureToConnect(const char* error) {
+    NSLog(@"FROM UNITY: failureToConnect(%s)", error);
+}
+void _failureToJoinRoom(const char* error) {
+    NSLog(@"FROM UNITY: failureToJoinRoom(%s)", error);
+}
+void _failureToConnectAgora(const char* error) {
+    NSLog(@"FROM UNITY: failureToConnectAgora(%s)", error);
+}
+
+
 }
