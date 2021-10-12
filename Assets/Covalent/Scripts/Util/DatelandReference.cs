40f67e4329a6669ed88c1898fb8d9cbaec657680
UltimateWalrus <ultimatewalrus@gmail.com>
2021-10-11 22:35:48 -0700

    Started setting up Go Karts scene.

---
+++ Assets/Covalent/Scripts/Util/DatelandReference.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Allows this to be controlled by ShowHideDatelandReference
+/// </summary>
+public class DatelandReference : MonoBehaviour
+{
+
+}
