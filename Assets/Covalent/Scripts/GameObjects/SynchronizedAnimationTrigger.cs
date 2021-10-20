6426caf0e8ff19d4da7b3655015a6fa6577aff85
UltimateWalrus <ultimatewalrus@gmail.com>
2021-10-20 13:04:36 -0700

    Added SynchronizedAnimationPosition component, which I think will do a better job of synchronizing arbitrarily large animations. Made an animation for the blue butterflies - next is to test if it net replicates correctly.

--- Assets/Covalent/Scripts/GameObjects/SynchronizedAnimationTrigger.cs
+++ Assets/Covalent/Scripts/GameObjects/SynchronizedAnimationTrigger.cs
@@ -6,20 +6,22 @@ using UnityEngine;
 
 /// <summary>
 /// Use an RPC to call an animation trigger on all clients periodically.
+/// If multiple triggerNames are provided, we'll call them one after the other,
+/// then loop around.
 /// </summary>
 public class SynchronizedAnimationTrigger : MonoBehaviourPun
 {
     public Animator animator;
-    public string triggerName = "start";
+    public string[] triggerNames;
     public float interval = 10.0f;
 
     float _cooldown;
-
+    int _nextIndex = 0;
 
     [PunRPC]
-    void DoTrigger()
+    void DoTrigger(int index)
     {
-        animator.SetTrigger(triggerName);
+        animator.SetTrigger(triggerNames[index]);
     }
 
 
@@ -30,8 +32,10 @@ public class SynchronizedAnimationTrigger : MonoBehaviourPun
             _cooldown = Mathf.Max( 0.0f, _cooldown - Time.fixedDeltaTime );
             if( _cooldown <= 0)
             {
-                photonView.RPC("DoTrigger", RpcTarget.All);   // Sets animation trigger on all clients, including us
+                photonView.RPC("DoTrigger", RpcTarget.All, new object[]{ _nextIndex });   // Sets animation trigger on all clients, including us
                 _cooldown = interval;   // reset cooldown
+
+                _nextIndex = (_nextIndex + 1) % triggerNames.Length;
             }
         }
     }
