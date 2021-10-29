1b9c2e0b303b6a6f210a2452db28dbb041fe4482
UltimateWalrus <ultimatewalrus@gmail.com>
2021-10-28 20:27:13 -0700

    Players now spawn in "Limbo" until their date joins, then they teleport to the designated spot in Rose Garden. Camera panning is disabled until their date has arrived. This avoids a bunch of non-controllable players waiting around in Rose Garden for their dates to show up.

---
+++ Assets/Covalent/Scripts/SceneLoading/Limbo.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Players wait here (outside of actual play bounds) until their date arrives.
+/// </summary>
+public class Limbo : MonoBehaviour
+{
+}
