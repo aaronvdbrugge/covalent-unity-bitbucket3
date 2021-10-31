906687b9950340d7cb5c4bfbebf95aca160921af
aaronvdbrugge <45329627+aaronvdbrugge@users.noreply.github.com>
2021-10-31 19:04:05 +0100

    [General] Add color property drawer, update gitignore for EditorUserSettings.asset
    Former-commit-id: ede387e266aaac5698a727f9be7ada4449630098

--- Assets/Covalent/Scripts/Util/Editor/ColorEditorOverride.cs
+++ Assets/Covalent/Scripts/Util/Editor/ColorEditorOverride.cs
@@ -4,25 +4,80 @@ using UnityEngine;
 [CustomPropertyDrawer(typeof(Color))]
 public class ColorEditorOverride : PropertyDrawer
 {
-	public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-	{
-		Debug.Log($"Hallo");
-		EditorGUI.BeginProperty(position, label, property);
-		position.width -= 80f;
-		EditorGUI.PropertyField(position, property, label);
-		Color color = property.colorValue;
-		var newP = new Rect(position);
-		newP.x += newP.width;
-		newP.width = 80f;
-		string hex = EditorGUI.TextField(newP, ColorUtility.ToHtmlStringRGBA(color));
-		if (ColorUtility.TryParseHtmlString("#" + hex, out Color newColor))
-		{
-			if (color != newColor)
-			{
-				property.colorValue = newColor;
-			}
-		}
-
-		EditorGUI.EndProperty();
-	}
+    private const int ColorMax = 4;
+    private SerializedProperty[] _rgba;
+    private Color _color = new Color();
+    private bool _isCached;
+    private string _colorStr;
+    private static readonly string[] Labels = {
+        "Red", "Green", "Blue", "Alpha"
+    };
+ 
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (!_isCached)
+        {
+            _rgba = new SerializedProperty[ColorMax];
+            _rgba[0] = property.FindPropertyRelative("r");
+            _rgba[1] = property.FindPropertyRelative("g");
+            _rgba[2] = property.FindPropertyRelative("b");
+            _rgba[3] = property.FindPropertyRelative("a");
+            _isCached = true;
+        }
+ 
+        position.height = EditorGUIUtility.singleLineHeight;
+        
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            for (var i = 0; i < ColorMax; i++)
+                _color[i] = _rgba[i].floatValue;
+            
+            _color = EditorGUI.ColorField(position, label, _color);
+            
+            if (check.changed)
+                for (var i = 0; i < ColorMax; i++)
+                    _rgba[i].floatValue = _color[i];
+        }
+ 
+        property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, GUIContent.none);
+        if (!property.isExpanded)
+            return;
+ 
+        using (new EditorGUI.IndentLevelScope(1))
+        {
+            using (var check = new EditorGUI.ChangeCheckScope())
+            {
+                for (var i = 0; i < ColorMax; i++)
+                {
+                    position.y += EditorGUIUtility.singleLineHeight;
+                    _color[i] = _rgba[i].floatValue;
+                    _color[i] = EditorGUI.Slider(position, Labels[i], _color[i], 0f, 1f);
+                }
+                if (check.changed)
+                    for (var i = 0; i < ColorMax; i++)
+                        _rgba[i].floatValue = _color[i];
+            }
+ 
+            position.y += EditorGUIUtility.singleLineHeight;
+            using (var check = new EditorGUI.ChangeCheckScope())
+            {
+                for (var i = 0; i < ColorMax; i++)
+                    _color[i] = _rgba[i].floatValue;
+                
+                _colorStr = EditorGUI.DelayedTextField(position, "HTML Color Code", ColorUtility.ToHtmlStringRGB(_color));
+                if (check.changed)
+                    if (ColorUtility.TryParseHtmlString("#" + _colorStr, out var color))
+                        for (var i = 0; i < ColorMax - 1; i++)
+                            _rgba[i].floatValue = color[i];
+            }
+        }
+    }
+ 
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        if (!property.isExpanded)
+            return EditorGUIUtility.singleLineHeight;
+        
+        return EditorGUIUtility.singleLineHeight * 6;
+    }
 }
