3a4ad9fa55baf7998caa540ee20d4ae388847bd9
aaronvdbrugge <45329627+aaronvdbrugge@users.noreply.github.com>
2021-10-31 23:33:51 +0100

    [Home Island] Add temporary debug tmpro element for clicking
    Former-commit-id: 989ea87ae5acaa130e238269f57139b200996b5a

--- Assets/Covalent/Scripts/UI/ClickFeature.cs
+++ Assets/Covalent/Scripts/UI/ClickFeature.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -5,26 +6,32 @@ namespace Covalent.Scripts.UI
 {
 	public class ClickFeature : MonoBehaviour, IPointerDownHandler, IPointerUpHandler, IPointerExitHandler
 	{
+		[SerializeField] private TextMeshProUGUI debugFeedbackTemp;
+		
 		private bool _tapping;
 		
 		public void OnPointerDown(PointerEventData eventData)
 		{
 			Debug.Log($"Finger down on: {gameObject.name}");
+			debugFeedbackTemp.text = $"Finger down on: {gameObject.name}";
 			_tapping = true;
 		}
 		
 		public void OnPointerUp(PointerEventData eventData)
 		{
 			Debug.Log($"Pointer up: {gameObject.name}");
+			debugFeedbackTemp.text = $"Pointer up: {gameObject.name}";
 			if (_tapping)
 			{
 				Debug.Log($"Trigger: {gameObject.name}");
+				debugFeedbackTemp.text = $"Trigger: {gameObject.name}";
 			}
 		}
 
 		public void OnPointerExit(PointerEventData eventData)
 		{
 			Debug.Log($"Pointer left: {gameObject.name}");
+			debugFeedbackTemp.text = $"Pointer left: {gameObject.name}";
 			_tapping = false;
 		}
 	}
