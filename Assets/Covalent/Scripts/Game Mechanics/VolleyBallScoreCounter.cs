db06b88b418e7100ab854559e75ecd9b4d4854ce
Sebastian Janisz <admin@ultimatewalrus.com>
2021-11-04 16:59:17 -0700

    Volleyball score counter now flashes green to start, then yellow at a hit streak of 8 or more, then red at a hit streak of 15 (max speed) or more.

--- Assets/Covalent/Scripts/Game Mechanics/VolleyBallScoreCounter.cs
+++ Assets/Covalent/Scripts/Game Mechanics/VolleyBallScoreCounter.cs
@@ -12,7 +12,14 @@ public class VolleyBallScoreCounter : MonoBehaviour
 {
 	public VolleyBall volleyball;
 	public TMP_Text tmpText;
-	public Color maxHitStreakColor;
+	[Tooltip("End of gradient is at max hit streak.")]
+	public Gradient flashColor;
+
+	public float flashTime = 0.25f;
+
+	float _flashCooldown;   // set to 1.0, counts down to 0
+	int _lastValue = int.MaxValue;   // last volleball.hitStreak
+	Color _flashColor;   // color for current flash
 
 	Color _originalTextColor;
 	private void Start()
@@ -23,10 +30,23 @@ public class VolleyBallScoreCounter : MonoBehaviour
 
 	private void Update()
 	{
-		tmpText.text = volleyball.hitStreak.ToString();
-		if( volleyball.hitStreak >= volleyball.maxSpeedupHits )   // It's at max speed! Color the text
-			tmpText.color = maxHitStreakColor;
-		else
-			tmpText.color = _originalTextColor;
+		if( volleyball.hitStreak != _lastValue )   // Need to display new number.
+		{
+			if( volleyball.hitStreak > _lastValue )  // went up! play a flash animation
+			{
+				_flashCooldown = 1.0f;
+				_flashColor = flashColor.Evaluate( Mathf.Clamp01(volleyball.hitStreak / (float)volleyball.maxSpeedupHits) );   // Pick an appropriate color based on how many times hit.
+			}
+			_lastValue = volleyball.hitStreak;
+			
+			tmpText.text = volleyball.hitStreak.ToString();
+		}
+
+
+		if( _flashCooldown > 0 )   // color the text
+		{
+			_flashCooldown -= Time.deltaTime / flashTime;
+			tmpText.color = Color.Lerp(_originalTextColor, _flashColor, _flashCooldown);
+		}
 	}
 }
