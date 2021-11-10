eb92341c07d88e5bfb2197d0d3284254d24c632f
UltimateWalrus <ultimatewalrus@gmail.com>
2021-11-10 13:58:57 -0800

    Added a new intermediate mode between "Debug" and "Release," which is "SRDebuggerOnly." This mode behaves as Release would, but still has SRDebugger which will cause a red exclamation point and log to appear in the case of an exception. This is necessary for testing / debugging.
    While Debug or SRDebuggerOnly are left on, for safety, I added a message to both loading screen and dateland welcome screen: "DEVELOPMENT BUILD, Do not distribute." This should prevent you from accidentally shipping with SRDebugger left on, so this resolves the red exclamation mark issue.
    
    
    Former-commit-id: f10f019f5d18a82e70881f2b08824388eaae1b7c

--- Assets/Covalent/Scripts/Debug/DestroyIfNotDebug.cs
+++ Assets/Covalent/Scripts/Debug/DestroyIfNotDebug.cs
@@ -9,9 +9,12 @@ public class DestroyIfNotDebug : MonoBehaviour
 {
     public DebugSettings debugSettings;
 
+    [Tooltip("Should we display this in SRDebuggerOnly mode? (used for DEVELOPMENT MODE warnings)")]
+    public bool srDebuggerOnlyOk = false;
+
     void Awake()
     {
-        if( debugSettings.mode != DebugSettings.BuildMode.Debug )
+        if( debugSettings.mode != DebugSettings.BuildMode.Debug && !(srDebuggerOnlyOk && debugSettings.mode == DebugSettings.BuildMode.SRDebuggerOnly) )
             Destroy( gameObject );
     }
 }
